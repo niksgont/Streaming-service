ec941e5c86472f9bfb243ec9cac1ab9879113316
Vikono <7klkarolink@gmail.com>
2023-04-23 17:34:44 +0300

    add category table

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.params import Depends, Path
 from tortoise.contrib.fastapi import register_tortoise
+import tortoise.contrib.pydantic
+# from tortoise. import
 from typing import List
-from schemas import FilmModel, FilmCreate, FilmUpdate, FilmInDB, Film
+from schemas import Films, FilmCreate, FilmUpdate, FilmInDB, Film, GetCategoryFilms, GetCategory
 import uvicorn
+import logging
 
 app = FastAPI()
+DATABASE_URL = "postgres://postgres:10272@localhost:5432/films"
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,24 +21,46 @@ app.add_middleware(
 )
 
 
+# @app.get("/films", response_model=List[GetCategoryFilms])
+# async def get_films():
+#     return await Films.all().prefetch_related("categories")
+
+# @app.get("/films", response_model=List[GetCategoryFilms])
+# async def get_films():
+#     films = await Films.all().prefetch_related("categories")
+#     return [await GetCategoryFilms.from_tortoise_orm(film) for film in films]
+
+async def get_film_with_categories(film: Films) -> GetCategoryFilms:
+    categories = await film.categories.all()
+    return GetCategoryFilms(
+        id=film.id,
+        title=film.title,
+        director=film.director,
+        year=film.year,
+        categories=[GetCategory.from_orm(category) for category in categories],
+    )
+
+
+@app.get("/films", response_model=List[GetCategoryFilms])
 async def get_films():
-    return await FilmModel.all()
+    films = await Films.all().prefetch_related("categories")
+    return [await get_film_with_categories(film) for film in films]
 
 
 async def get_film(film_id: int):
-    film = await FilmModel.get_or_none(id=film_id)
+    film = await Films.get_or_none(id=film_id).prefetch_related("categories")
     if film is None:
         raise HTTPException(status_code=404, detail="Film not found")
     return film
 
 
 async def create_film(film: FilmCreate):
-    new_film = await FilmModel.create(**film.dict())
+    new_film = await Films.create(**film.dict())
     return new_film
 
 
 async def delete_film(film_id: int):
-    film = await FilmModel.get_or_none(id=film_id)
+    film = await Films.get_or_none(id=film_id)
     if film is None:
         raise HTTPException(status_code=404, detail="Film not found")
     await film.delete()
@@ -59,10 +85,11 @@ async def create_film_endpoint(film: FilmCreate):
 register_tortoise(
     app,
     db_url=DATABASE_URL,
-    modules={"models": ["main"]},
+    modules={"models": ["schemas"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
--- schemas.py
+++ schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import PydanticListModel, PydanticModel
+from typing import List
 
 
 class FilmCreate(BaseModel):
@@ -21,12 +23,34 @@ class Film(FilmInDB):
     pass
 
 
-class FilmModel(Model):
-    id = fields.IntField(pk=True)
+class GetCategory(PydanticModel):
+    category_name: str
+
+
+class GetCategoryFilms(PydanticModel):
+    id: int
+    title: str
+    director: str
+    year: int
+    categories: List[GetCategory] = []
+
+
+class Films(Model):
+    id = fields.IntField(pk=True)  # prob unique is extra
     title = fields.CharField(max_length=255)
     director = fields.CharField(max_length=255)
     year = fields.IntField()
+    categories: fields.ReverseRelation["Category"]
 
-    class Meta:
-        table = "films"
+    # class PydanticMeta:
+    #     table = "films"
+
+
+class Category(Model):
+    category_name = fields.CharField(max_length=255, pk=True)
+    film: fields.ForeignKeyRelation[Films] = \
+        fields.ForeignKeyField("models.Films", related_name='categories')
+
+    # class PydanticMeta:
+    #     table = "category"
 
